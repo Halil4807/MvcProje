3941f4a1c5256d9323aa148837f5b70c9dd6a011
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-06 01:42:31 +0300

    ürün Günceleme alanının yapımı

--- Controllers/UrunlerController.cs
+++ Controllers/UrunlerController.cs
@@ -41,15 +41,23 @@ namespace MvcProje.Controllers
         }
         public ActionResult UrunGuncelle(int id)
         {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList() select new SelectListItem { Text = i.KATEGORIAD, Value = i.KATEGORIID.ToString() }).ToList();
+            ViewBag.ktg = degerler;
             var urun = db.TBLURUNLER.Find(id);
             return View("UrunGuncelle", urun);
         }
+
         public ActionResult Guncelle(TBLURUNLER parametre)
         {
             var urun = db.TBLURUNLER.Find(parametre.URUNID);
             urun.URUNAD = parametre.URUNAD;
+            urun.MARKA = parametre.MARKA;
+            //urun.URUNKATEGORI = parametre.URUNKATEGORI;
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == parametre.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            urun.URUNKATEGORI = ktg.KATEGORIID;
+            urun.FIYAT = parametre.FIYAT;
+            urun.STOK = parametre.STOK;
             db.SaveChanges();
-
             return RedirectToAction("Index");
         }
     }
