36800cb28028a34f8606081500c13534d3336e59
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-06 03:30:49 +0300

    POPUP Ekleme
    Satış yapma ve hakkımızda sayfası

---
+++ Controllers/SatisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcProje.Models.Entity;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class SatisController : Controller
+    {
+
+        // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR parametre)
+        {
+            db.TBLSATISLAR.Add(parametre);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
